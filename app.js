554233120a243cd276bd9785f8ed53c805099336
arif <moarifur@gmail.com>
2021-06-28 22:15:23 +0600

    Class#02: JS variable and three types of variable: (i) number (ii) string (iii) object

---
+++ app.js
@@ -0,0 +1,73 @@
+/* ----------------------- Software Development Life Cycle -------------------------------------
+    - Requirement analysis(15%)
+    - Designing(35%)
+    - Development(15%)
+        Development 2 Phase: 
+            - FrontEnd
+            - BackEnd
+    - Deployment
+    - Testing(20%)
+    - Deployment(Complete Phase) (15%)
+*/ 
+
+/* ------------------------------- Why JavaScript? ----------------------------------------------
+    - UI/UX: React/ Angular/ Vue
+    - DB: MongoDB
+    - Server: NodeJS
+    - Controller: Express
+    - Mobile: React Native - Android + iOS - Hybride
+
+    Basic JS: ES6
+*/ 
+
+/* -------------- Assignment: ---------------------
+ - PresentationBased: What is ES6?
+ - Github: Create new account
+*/
+
+/*
+    Programming Language
+    - Conditional Operation
+    - Iterative Operation
+
+    Unit of Work: function - Code Block
+*/ 
+
+// What is variable
+/*
+    Types of variable:
+    - integer: 1,2,3
+    - String: I am taking this JS class for my students, akjskahdkjahkjd, #AC0056, ...
+    - array - [1, 'a', 'Jhon']
+    - object - {}
+
+    let / const
+*/ 
+
+// Define a variable
+let number = 5
+// console.log(number)
+number = 6
+console.log(number)
+console.log(typeof number)
+
+number = 'Five'
+console.log(number)
+console.log(typeof number)
+
+number = '$'
+console.log(number)
+console.log(typeof number)
+
+number = 2.33
+console.log(number)
+console.log(typeof number)
+
+number = [2.33, 5]
+console.log(number)
+console.log(typeof number)
+
+// So far we learn this:
+// JS variable and three types of variable: (i) number (ii) string (iii) object 
+
+// Assignment: PresentationBased - JS variable and it's use
