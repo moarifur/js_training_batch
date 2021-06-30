01097baecd75cb19a72c2db7c7d6220b31f97367
arif <moarifur@gmail.com>
2021-06-30 22:57:06 +0600

    Class#03: Total Recap

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
+// ----------- Class#01: Basic Software development Strategy ----------------------------------- 
+
 /* ----------------------- Software Development Life Cycle -------------------------------------
     - Requirement analysis(15%)
     - Designing(35%)
     - Development(15%)
         Development 2 Phase: 
-            - FrontEnd
-            - BackEnd
+            - FrontEnd: UI
+            - BackEnd: Server + Database + BL(Business Logic)
     - Deployment
     - Testing(20%)
     - Deployment(Complete Phase) (15%)
 */ 
 
 /* ------------------------------- Why JavaScript? ----------------------------------------------
-    - UI/UX: React/ Angular/ Vue
+    BackEnd Developer
     - DB: MongoDB
     - Server: NodeJS
-    - Controller: Express
+    - BL: Express
+    FrontEnd Developer
+    - UI/UX: React/ Angular/ Vue
+    App Developer
     - Mobile: React Native - Android + iOS - Hybride
-
+    
+    Fullstack Developer: FrontEnd Developer + BackEnd Developer
+    
     Basic JS: ES6
 */ 
 
@@ -25,6 +32,8 @@
  - Github: Create new account
 */
 
+
+// ----------- Class#02: JS variable and three types of variable: (i) number (ii) string (iii) object 
 /*
     Programming Language
     - Conditional Operation
@@ -55,7 +64,7 @@ number = 'Five'
 console.log(number)
 console.log(typeof number)
 
-number = '$'
+number = '5'
 console.log(number)
 console.log(typeof number)
 
@@ -71,3 +80,50 @@ console.log(typeof number)
 // JS variable and three types of variable: (i) number (ii) string (iii) object 
 
 // Assignment: PresentationBased - JS variable and it's use
+
+console.log()
+
+/*
+    Function: Unit of work: DRY(Don't Repeat Yourself)
+    - Built-in func : console.log()
+    - User Defined func: calcTemp()
+
+    Function
+    - Should be Defined
+    - Should be Called
+*/ 
+
+// function defination
+function printAnzuApu() {
+    console.log('Anzu Apu')
+}
+
+// function call
+printAnzuApu()
+
+
+/*
+    Learning so far:
+    // 1. console.log
+    // 2. Variable
+    3. function
+    // 4. ES6
+    // 5. JS AIO
+    // 6. Data type: number, string, object
+    // 7. Software life cycle
+    // 8. FrontEnd and BackEnd
+    // 9. Why JavaScript?
+
+    x = 1
+    x = 2
+    Variable = container
+    variable ki contain kore? - data
+    data = data structure
+    koto typer data ache? - number, string, object, Null, undefined, NAN, boolean
+    variable kivabe define korbo? - let, const, var
+    variable kivabe likhbo? - add_two_numbers, addTwoNumbers, AddTwoNumbers
+
+
+
+*/ 
+
